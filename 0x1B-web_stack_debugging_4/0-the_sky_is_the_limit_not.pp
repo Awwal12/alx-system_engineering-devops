dcd8eba77f5c26b0a550e6937bb8e9ec90842f95
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-08-14 13:44:26 +0100

    first task

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,12 @@
-# Increse the request's limit
+# Fix problem of high amount of requests
 
-# Increse ulimit value
-exec { 'fix-config-nginx':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
-# Restart nginex service
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
